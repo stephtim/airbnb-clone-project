2bdb647dece371c7e61c0f5434c60123255ca209
stephtim <38559257+stephtim@users.noreply.github.com>
2025-09-02 13:36:35 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -25,3 +25,21 @@ GROUP BY
     p.property_id, p.title
 ORDER BY 
     booking_position;
+
+# window function
+SELECT 
+    p.property_id,
+    p.title,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM 
+    Properties p
+LEFT JOIN 
+    Bookings b 
+ON 
+    p.property_id = b.property_id
+GROUP BY 
+    p.property_id, p.title
+ORDER BY 
+    booking_rank;
+
